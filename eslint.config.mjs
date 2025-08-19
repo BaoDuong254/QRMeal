f1253883dc568df5572b62f4fdb31ad7b36475e5
GiaBao <duonggiabao254@gmail.com>
2025-08-19 15:31:19 +0700

    feat: integrate Prettier for code formatting and add configuration files

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,6 +1,7 @@
 import { dirname } from "path";
 import { fileURLToPath } from "url";
 import { FlatCompat } from "@eslint/eslintrc";
+import eslintPluginPrettier from "eslint-plugin-prettier";
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
@@ -11,6 +12,28 @@ const compat = new FlatCompat({
 
 const eslintConfig = [
   ...compat.extends("next/core-web-vitals", "next/typescript"),
+  {
+    files: ["**/*.{ts,tsx}"],
+    plugins: {
+      prettier: eslintPluginPrettier,
+    },
+    rules: {
+      "prettier/prettier": [
+        "warn",
+        {
+          arrowParens: "always",
+          semi: true,
+          trailingComma: "es5",
+          tabWidth: 2,
+          endOfLine: "auto",
+          useTabs: false,
+          singleQuote: false,
+          printWidth: 120,
+          jsxSingleQuote: true,
+        },
+      ],
+    },
+  },
 ];
 
 export default eslintConfig;

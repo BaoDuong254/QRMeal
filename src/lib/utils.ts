2f9ec29f11dc79daa69487a347964c3d28a8003d
GiaBao <duonggiabao254@gmail.com>
2025-08-19 15:16:01 +0700

    feat: - implement schema validations for accounts, orders, dishes, and guests using Zod - implement http client to call API - add constants/type file

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,6 +1,20 @@
-import { clsx, type ClassValue } from "clsx"
-import { twMerge } from "tailwind-merge"
+import { clsx, type ClassValue } from "clsx";
+import { twMerge } from "tailwind-merge";
 
 export function cn(...inputs: ClassValue[]) {
-  return twMerge(clsx(inputs))
+	return twMerge(clsx(inputs));
 }
+
+/**
+ * Normalizes a path by removing leading slashes.
+ * 
+ * @param path Normalizes a path by removing leading slashes.
+ * @returns The normalized path without leading slashes.
+ * 
+ * @example
+ * normalizePath("/api/auth/login") // returns "api/auth/login"
+ * normalizePath("api/auth/login") // returns "api/auth/login"
+ */
+export const normalizePath = (path: string) => {
+	return path.startsWith("/") ? path.slice(1) : path;
+};

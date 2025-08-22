727df803958bfffd1ca442c756f776f8eace0b93
GiaBao <duonggiabao254@gmail.com>
2025-08-22 22:21:13 +0700

    feat: add change password functionality with API integration and form handling

--- src/app/manage/setting/ChangePasswordForm.tsx
+++ src/app/manage/setting/ChangePasswordForm.tsx
@@ -8,8 +8,12 @@ import { useForm } from "react-hook-form";
 import { ChangePasswordBody, ChangePasswordBodyType } from "@/schemaValidations/account.schema";
 import { zodResolver } from "@hookform/resolvers/zod";
 import { Form, FormField, FormItem, FormMessage } from "@/components/ui/form";
+import { useChangePasswordMutation } from "@/queries/useAccount";
+import { toast } from "sonner";
+import { handleErrorApi } from "@/lib/utils";
 
 export default function ChangePasswordForm() {
+  const changePasswordMutation = useChangePasswordMutation();
   const form = useForm<ChangePasswordBodyType>({
     resolver: zodResolver(ChangePasswordBody),
     defaultValues: {
@@ -18,10 +22,27 @@ export default function ChangePasswordForm() {
       confirmPassword: "",
     },
   });
+  const onReset = () => {
+    form.reset();
+  };
+  const onSubmit = async (data: ChangePasswordBodyType) => {
+    if (changePasswordMutation.isPending) return;
+    try {
+      const result = await changePasswordMutation.mutateAsync(data);
+      toast(result.payload.message);
+    } catch (error) {
+      handleErrorApi({ error, setError: form.setError });
+    }
+  };
 
   return (
     <Form {...form}>
-      <form noValidate className='grid auto-rows-max items-start gap-4 md:gap-8'>
+      <form
+        noValidate
+        className='grid auto-rows-max items-start gap-4 md:gap-8'
+        onSubmit={form.handleSubmit(onSubmit)}
+        onReset={onReset}
+      >
         <Card className='overflow-hidden' x-chunk='dashboard-07-chunk-4'>
           <CardHeader>
             <CardTitle>Đổi mật khẩu</CardTitle>
@@ -35,7 +56,13 @@ export default function ChangePasswordForm() {
                   <FormItem>
                     <div className='grid gap-3'>
                       <Label htmlFor='oldPassword'>Mật khẩu cũ</Label>
-                      <Input id='oldPassword' type='password' className='w-full' {...field} />
+                      <Input
+                        id='oldPassword'
+                        type='password'
+                        className='w-full'
+                        {...field}
+                        autoComplete='current-password'
+                      />
                       <FormMessage />
                     </div>
                   </FormItem>
@@ -48,7 +75,7 @@ export default function ChangePasswordForm() {
                   <FormItem>
                     <div className='grid gap-3'>
                       <Label htmlFor='password'>Mật khẩu mới</Label>
-                      <Input id='password' type='password' className='w-full' {...field} />
+                      <Input id='password' type='password' className='w-full' {...field} autoComplete='new-password' />
                       <FormMessage />
                     </div>
                   </FormItem>
@@ -61,17 +88,25 @@ export default function ChangePasswordForm() {
                   <FormItem>
                     <div className='grid gap-3'>
                       <Label htmlFor='confirmPassword'>Nhập lại mật khẩu mới</Label>
-                      <Input id='confirmPassword' type='password' className='w-full' {...field} />
+                      <Input
+                        id='confirmPassword'
+                        type='password'
+                        className='w-full'
+                        {...field}
+                        autoComplete='new-password'
+                      />
                       <FormMessage />
                     </div>
                   </FormItem>
                 )}
               />
               <div className='flex items-center gap-2 md:ml-auto'>
-                <Button variant='outline' size='sm'>
+                <Button variant='outline' size='sm' type='reset'>
                   Hủy
                 </Button>
-                <Button size='sm'>Lưu thông tin</Button>
+                <Button size='sm' type='submit'>
+                  Lưu thông tin
+                </Button>
               </div>
             </div>
           </CardContent>

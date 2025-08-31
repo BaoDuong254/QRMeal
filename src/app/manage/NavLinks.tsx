9ee09a5d6600697508e988f2b8351702bc0a3a63
GiaBao <duonggiabao254@gmail.com>
2025-08-31 12:01:29 +0700

    feat: add recharts for data visualization and implement dashboard charts
    - Added `recharts` library to package.json for charting capabilities.
    - Updated global CSS to define color variables for charts.
    - Refactored MobileNavLinks and NavLinks to update hrefs for navigation.
    - Replaced the existing dashboard page with a new layout using `DashboardMain` component.
    - Created `DashboardMain` component to encapsulate dashboard functionality and charts.
    - Implemented `DishBarChart` and `RevenueLineChart` components for visualizing dish popularity and revenue trends.
    - Enhanced card components for better layout and styling in the dashboard.

--- src/app/manage/NavLinks.tsx
+++ src/app/manage/NavLinks.tsx
@@ -14,7 +14,7 @@ export default function NavLinks() {
       <aside className='bg-background fixed inset-y-0 left-0 z-10 hidden w-14 flex-col border-r sm:flex'>
         <nav className='flex flex-col items-center gap-4 px-2 py-4'>
           <Link
-            href='#'
+            href='/'
             className='group bg-primary text-primary-foreground flex h-9 w-9 shrink-0 items-center justify-center gap-2 rounded-full text-lg font-semibold md:h-8 md:w-8 md:text-base'
           >
             <Package2 className='h-4 w-4 transition-all group-hover:scale-110' />

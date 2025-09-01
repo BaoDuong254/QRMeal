a7de9762699e48e15afd3eee4f03fb2240887e33
GiaBao <duonggiabao254@gmail.com>
2025-09-02 00:12:03 +0700

    refactor: simplify parameter destructuring in DishPage component

--- src/app/(public)/@modal/(.)dishes/[id]/page.tsx
+++ src/app/(public)/@modal/(.)dishes/[id]/page.tsx
@@ -3,8 +3,14 @@ import Modal from "@/app/(public)/@modal/(.)dishes/[id]/Modal";
 import DishDetail from "@/app/(public)/dishes/[id]/DishDetail";
 import { wrapServerApi } from "@/lib/utils";
 
-export default async function DishPage(props: { params: { id: string } }) {
-  const { id } = await props.params;
+export default async function DishPage({
+  params,
+}: {
+  params: Promise<{
+    id: string;
+  }>;
+}) {
+  const { id } = await params;
   const data = await wrapServerApi(() => dishApiRequest.getDish(Number(id)));
 
   const dish = data?.payload?.data;

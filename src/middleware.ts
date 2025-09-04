0db94fcbbc4e517af458a236e4c02418d3503876
GiaBao <duonggiabao254@gmail.com>
2025-09-04 22:38:32 +0700

    feat: refine middleware logic for authentication and role-based access control; improve comments for clarity

--- src/middleware.ts
+++ src/middleware.ts
@@ -17,72 +17,55 @@ const privatePaths = [...managePaths, ...guestPaths];
 const unAuthPaths = ["/vi/login", "/en/login"];
 const loginPaths = ["/vi/login", "/en/login"];
 
-// This function can be marked `async` if using `await` inside
 export function middleware(request: NextRequest) {
   const handleI18nRouting = createMiddleware(routing);
   const response = handleI18nRouting(request);
   const { pathname, searchParams } = request.nextUrl;
-  // pathname: /manage/dashboard
   const accessToken = request.cookies.get("accessToken")?.value;
   const refreshToken = request.cookies.get("refreshToken")?.value;
   const locale = request.cookies.get("NEXT_LOCALE")?.value ?? defaultLocale;
-  // 1. Chưa đăng nhập thì không cho vào private paths
+  // If no login, redirect to login page
   if (privatePaths.some((path) => pathname.startsWith(path)) && !refreshToken) {
     const url = new URL(`/${locale}/login`, request.url);
     url.searchParams.set("clearTokens", "true");
     return NextResponse.redirect(url);
-    // response.headers.set('x-middleware-rewrite', url.toString())
-    // return response
   }
-  // 2. Trường hợp đã đăng nhập
+
+  // If have login
   if (refreshToken) {
-    // 2.1 Nếu cố tình vào trang login sẽ redirect về trang chủ
+    // If enter login page but already have token, redirect to home page
     if (unAuthPaths.some((path) => pathname.startsWith(path))) {
       if (loginPaths.some((path) => pathname.startsWith(path)) && searchParams.get("accessToken")) {
         return response;
       }
       return NextResponse.redirect(new URL(`/${locale}`, request.url));
-      // response.headers.set(
-      //   'x-middleware-rewrite',
-      //   new URL('/en', request.url).toString()
-      // )
-      // return response
     }
 
-    // 2.2 Nhưng access token lại hết hạn
+    // accessToken expired, redirect to refresh token
     if (privatePaths.some((path) => pathname.startsWith(path)) && !accessToken) {
       const url = new URL(`/${locale}/refresh-token`, request.url);
       url.searchParams.set("refreshToken", refreshToken);
       url.searchParams.set("redirect", pathname);
       return NextResponse.redirect(url);
-      // response.headers.set('x-middleware-rewrite', url.toString())
-      // return response
     }
 
-    // 2.3 Vào không đúng role, redirect về trang chủ
+    // Check role
     const role = decodeToken(refreshToken).role;
-    // Guest nhưng cố vào route owner
+    // Guest but enter manage path
     const isGuestGoToManagePath = role === Role.Guest && managePaths.some((path) => pathname.startsWith(path));
-    // Không phải Guest nhưng cố vào route guest
+    // Not Guest but enter guest path
     const isNotGuestGoToGuestPath = role !== Role.Guest && guestPaths.some((path) => pathname.startsWith(path));
-    // Không phải Owner nhưng cố tình truy cập vào các route dành cho owner
+    // Not Owner but enter only owner path
     const isNotOwnerGoToOwnerPath = role !== Role.Owner && onlyOwnerPaths.some((path) => pathname.startsWith(path));
     if (isGuestGoToManagePath || isNotGuestGoToGuestPath || isNotOwnerGoToOwnerPath) {
       return NextResponse.redirect(new URL(`/${locale}`, request.url));
-      // response.headers.set(
-      //   'x-middleware-rewrite',
-      //   new URL('/', request.url).toString()
-      // )
-      // return response
     }
 
-    // return NextResponse.next()
     return response;
   }
   return response;
 }
 
-// See "Matching Paths" below to learn more
 export const config = {
   matcher: ["/", "/(vi|en)/:path*"],
 };

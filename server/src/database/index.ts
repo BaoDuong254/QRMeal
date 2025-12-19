46679521b25f9b1edc4527e03b7d8061272882a8
GiaBao <duonggiabao254@gmail.com>
2025-12-20 02:02:02 +0700

    chore: update pnpm workspace and Dockerfile for Prisma 5.22.0

--- server/src/database/index.ts
+++ server/src/database/index.ts
@@ -1,12 +1,6 @@
-import envConfig from "@/config";
-import { PrismaBetterSqlite3 } from "@prisma/adapter-better-sqlite3";
 import { PrismaClient } from "@generated/prisma/client";
 
-const connectionString = envConfig.DATABASE_URL;
-const adapter = new PrismaBetterSqlite3({ url: connectionString });
-
 const prisma = new PrismaClient({
-  adapter,
   log: ["info"],
 });
 
